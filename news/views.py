5dd8d14499474d205c447eaa6712688bef974db4
Mariana Pereira <marianapereira.s@live.com>
2024-05-21 22:45:26 -0300

    Criação da Home Page

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
+
+
+def home_page(request):
+    news_list = News.objects.all()
+    return render(request, 'home.html', {
+        'news_list': news_list
+    })
